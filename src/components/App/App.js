faf21fd8b98ecdfc1bd58da874d20c0ce6e5ec20
Myron <dubtwee@gmail.com>
2020-07-21 09:31:30 -0500

    initial codebase

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+// CUSTOM COMPONENTS
+import AppLayout from '../AppLayout/AppLayout';
+
+class App extends React.Component {
+  render() {
+    return (
+      <AppLayout>
+        {/* ADDITIONAL CONTENT */}
+        BODY CONTENT
+      </AppLayout>
+    );
+  }
+}
+
+export default App;
